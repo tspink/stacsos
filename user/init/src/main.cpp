fb20e6c905dd661222cc247ed30435449df910cb
Tom Spink <tcs6@st-andrews.ac.uk>
2024-10-01 11:50:56 +0100

    Make the init process display a logo if the console is in gfx mode

--- user/init/src/main.cpp
+++ user/init/src/main.cpp
@@ -16,66 +16,67 @@ using namespace stacsos;
 
 static void logo()
 {
-#ifndef GFX_LOGO
-	console::get().write("\e\xb0                                                                                ");
-	console::get().write("\e\xb0                             Welcome to StACSOS!                                ");
-	console::get().write("\e\xb0                                                                                ");
-#else
-	console::get().write("\n\n\n\n");
-
-	auto logo_file = object::open("/logo.ppm");
-	if (!logo_file) {
-		console::get().write("logo not found\n");
-	}
-
-	u8 *logo_data = new u8[170415];
-	logo_file->read(logo_data, 170415);
-	delete logo_file;
-
-	logo_data++; // 0x50 P
-	logo_data++; // 0x36 6
-	logo_data++; // 0x0a
-
-	int width = 0;
-	while (*logo_data != 0x20) {
-		width *= 10;
-		width += *logo_data++ - '0';
-	}
-
-	logo_data++;
-
-	int height = 0;
-	while (*logo_data != 0xa) {
-		height *= 10;
-		height += *logo_data++ - '0';
-	}
-
-	logo_data++;
-
-	int maxval = 0;
-	while (*logo_data != 0xa) {
-		maxval *= 10;
-		maxval += *logo_data++ - '0';
-	}
-
-	logo_data++;
-
 	object *fb = object::open("/dev/virtcon0");
-	const u8 *pixel_data = (const u8 *)logo_data;
 
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			u32 data_pixel_offset = (x + (y * width)) * 3;
-			u32 screen_pixel_offset = (x + ((640 - width) / 2)) + (y * 640);
+	int console_mode = (int)fb->ioctl(1, nullptr, 0);
+	if (console_mode == 0) {
+		console::get().write("\e\xb0                                                                                ");
+		console::get().write("\e\xb0                             Welcome to StACSOS!                                ");
+		console::get().write("\e\xb0                                                                                ");
+	} else {
+		console::get().write("\n\n\n\n");
 
-			u32 pixel = pixel_data[data_pixel_offset] << 16 | pixel_data[data_pixel_offset + 1] << 8 | pixel_data[data_pixel_offset + 2] << 0;
+		auto logo_file = object::open("/logo.ppm");
+		if (!logo_file) {
+			console::get().write("logo not found\n");
+		}
 
-			fb->pwrite((const void *)&pixel, 4, screen_pixel_offset);
+		u8 *logo_data = new u8[170415];
+		logo_file->read(logo_data, 170415);
+		delete logo_file;
+
+		logo_data++; // 0x50 P
+		logo_data++; // 0x36 6
+		logo_data++; // 0x0a
+
+		int width = 0;
+		while (*logo_data != 0x20) {
+			width *= 10;
+			width += *logo_data++ - '0';
+		}
+
+		logo_data++;
+
+		int height = 0;
+		while (*logo_data != 0xa) {
+			height *= 10;
+			height += *logo_data++ - '0';
+		}
+
+		logo_data++;
+
+		int maxval = 0;
+		while (*logo_data != 0xa) {
+			maxval *= 10;
+			maxval += *logo_data++ - '0';
+		}
+
+		logo_data++;
+
+		object *fb = object::open("/dev/virtcon0");
+		const u8 *pixel_data = (const u8 *)logo_data;
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				u32 data_pixel_offset = (x + (y * width)) * 3;
+				u32 screen_pixel_offset = (x + ((640 - width) / 2)) + (y * 640);
+
+				u32 pixel = pixel_data[data_pixel_offset] << 16 | pixel_data[data_pixel_offset + 1] << 8 | pixel_data[data_pixel_offset + 2] << 0;
+
+				fb->pwrite((const void *)&pixel, 4, screen_pixel_offset);
+			}
 		}
 	}
-
-	delete fb;
-#endif
 }
 
 int main(const char *cmdline)
