ffd8a658d1962c94f1477ce8a3f671d4a311786c
Tom Spink <tcs6@st-andrews.ac.uk>
2025-08-13 12:19:21 +0100

    Add a very basic formatting mode to the `cat` tool

--- user/cat/src/main.cpp
+++ user/cat/src/main.cpp
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: MIT */
 
-/* StACSOS - cat utility
+/* StACSOS - cat utility, with extremely basic formatting utility.
  *
- * Copyright (c) University of St Andrews 2024
+ * Copyright (c) University of St Andrews 2024, 2025
  * Tom Spink <tcs6@st-andrews.ac.uk>
  */
 #include <stacsos/console.h>
@@ -14,9 +14,29 @@ using namespace stacsos;
 int main(const char *cmdline)
 {
 	if (!cmdline || memops::strlen(cmdline) == 0) {
-		console::get().write("error: usage: cat <filename>\n");
+		console::get().write("error: usage: cat [-f] <filename>\n");
 		return 1;
 	}
+
+	bool formatting_mode = false;
+	while (*cmdline) {
+		if (*cmdline == '-') {
+			cmdline++;
+
+			if (*cmdline++ == 'f') {
+				formatting_mode = true;
+			} else {
+				console::get().write("error: usage: cat [-f] <filename>\n");
+				return 1;
+			}
+		} else {
+			break;
+		}
+	}
+
+	while (*cmdline == ' ') {
+		cmdline++;
+	};
 
 	object *file = object::open(cmdline);
 	if (!file) {
@@ -27,11 +47,33 @@ int main(const char *cmdline)
 	char buffer[64];
 	int bytes_read;
 
+	bool coloured = false;
+
 	do {
 		bytes_read = file->read(buffer, sizeof(buffer) - 1);
 		buffer[bytes_read] = 0;
 
-		console::get().writef("%s", buffer);
+		if (formatting_mode) {
+			char *ch = &buffer[0];
+			while (*ch) {
+				if (*ch == '`') {
+					coloured = !coloured;
+					if (coloured) {
+						console::get().write("\e\x0e");
+					}
+
+					console::get().writef("%c", *ch++);
+
+					if (!coloured) {
+						console::get().write("\e\x07");
+					}
+				} else {
+					console::get().writef("%c", *ch++);
+				}
+			}
+		} else {
+			console::get().writef("%s", buffer);
+		}
 	} while (bytes_read > 0);
 
 	delete file;
